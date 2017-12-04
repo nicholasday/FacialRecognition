7d155dd83ac18be499f52045f3b5b5c816e7faff
pwnham <mohammad.haveliwala@gmail.com>
2017-12-04 01:24:21 -0500

    t

--- src/facialrecognitionjava/Label.java
+++ src/facialrecognitionjava/Label.java
@@ -23,4 +23,44 @@ public class Label {
         this.color = color;
         this.size = size;
     }
+    
+     public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Scalar getColor() {
+        return color;
+    }
+
+    public void setColor(Scalar color) {
+        this.color = color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
