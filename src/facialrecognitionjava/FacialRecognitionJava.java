3f67dd574f3cb5f92da8558c35d918b268db04f2
pwnham <mohammad.haveliwala@gmail.com>
2017-12-04 00:20:06 -0500

    h

--- src/facialrecognitionjava/FacialRecognitionJava.java
+++ src/facialrecognitionjava/FacialRecognitionJava.java
@@ -34,7 +34,6 @@ public class FacialRecognitionJava {
         } 
         System.out.println(System.getProperty("os.name"));
 
-        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("mat = " + mat.dump());
     }
---
+++ src/facialrecognitionjava/Label.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facialrecognitionjava;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+/**
+ *
+ * @author moham
+ */
+public class Label {
+    private double x, y;
+    private String text;
+    private Scalar color;
+    private int size;
+    
+    public Label(double x, double y, String text, Scalar color, int size) {
+        
+    }
+}
