32cd541f8ec96140e105f4e10ba4fa4a9637da83
Nicholas Day <nick@tutorialsbynick.com>
2017-12-03 23:26:08 -0500

    Added java

--- src/facialrecognitionjava/FacialRecognitionJava.java
+++ src/facialrecognitionjava/FacialRecognitionJava.java
@@ -5,6 +5,11 @@
  */
 package facialrecognitionjava;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import java.io.File;
+
 /**
  *
  * @author nicholas
@@ -15,6 +20,16 @@ public class FacialRecognitionJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String os = System.getProperty("os.name");
+        if (os.toUpperCase().contains("LINUX")) {
+            File lib = new File("libs/" + System.mapLibraryName("opencv_java331"));     
+            System.out.println(lib.getAbsolutePath());
+            System.load(lib.getAbsolutePath());
+        }
+
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
         // TODO code application logic here
     }
     
