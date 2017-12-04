a79550da16fa1d6aa83878a8bc3d87b85c66db12
pwnham <mohammad.haveliwala@gmail.com>
2017-12-03 23:42:48 -0500

    Added mac

--- src/facialrecognitionjava/FacialRecognitionJava.java
+++ src/facialrecognitionjava/FacialRecognitionJava.java
@@ -9,10 +9,8 @@ package facialrecognitionjava;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-<<<<<<< HEAD
-=======
+
 import java.io.File;
->>>>>>> 32cd541f8ec96140e105f4e10ba4fa4a9637da83
 
 /**
  *
@@ -29,16 +27,16 @@ public class FacialRecognitionJava {
             File lib = new File("libs/" + System.mapLibraryName("opencv_java331"));     
             System.out.println(lib.getAbsolutePath());
             System.load(lib.getAbsolutePath());
-        }
+        }  else if (os.toUpperCase().contains("MAC OS X")) {
+            File lib = new File("libs/" + System.mapLibraryName("opencv_java331"));     
+            System.out.println(lib.getAbsolutePath());
+            System.load(lib.getAbsolutePath());
+        } 
+        System.out.println(System.getProperty("os.name"));
 
         //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("mat = " + mat.dump());
-        // TODO code application logic here
-          System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-  Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-                System.out.println("mat = " + mat.dump());
-        
     }
     
 }
